1df87b1a755ebc68116dc4379f1490fdecc7b3aa
Kevin Sajan <kevsaj@gmail.com>
2020-12-08 23:02:38 -0500

    idea

---
+++ script.js
@@ -0,0 +1,12 @@
+var $currentDay = $("#currentDay");
+var $timeBlocks = $(".time-block");
+var $scheduleArea = $(".schedule");
+
+var toDoItems = [];
+
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");
+
+function initializeSchedule() {
+    
+}
